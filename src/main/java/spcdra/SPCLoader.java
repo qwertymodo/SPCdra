b0e92bfbdce505d556836435ad015ae941bc63cc
qwertymodo <qwertymodo@qwertymodo.com>
2025-02-07 21:18:14 -0800

    Fix IllegalArgumentException when upgrading project to 11.3

--- src/main/java/spcdra/SPCLoader.java
+++ src/main/java/spcdra/SPCLoader.java
@@ -53,10 +53,16 @@ public class SPCLoader extends AbstractLibrarySupportLoader {
 	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
 		List<LoadSpec> loadSpecs = new ArrayList<>();
 
-		if(new String(provider.getInputStream(0).readNBytes(33),0,33).compareTo("SNES-SPC700 Sound File Data v0.30") != 0)
-			return loadSpecs;
+		// TODO: Examine the bytes in 'provider' to determine if this loader can load it.  If it 
+		// can load it, return the appropriate load specifications.
 		
-		loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("spc700:LE:16:default", "default"), true));
+		try {
+			if(new String(provider.getInputStream(0).readNBytes(33),0,33).compareTo("SNES-SPC700 Sound File Data v0.30") != 0)
+				return loadSpecs;
+			
+			loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("spc700:LE:16:default", "default"), true));
+		} catch(IllegalArgumentException e) {}
+		
 		return loadSpecs;
 	}
 
